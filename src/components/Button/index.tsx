78837b47544d3e57af3d02d67aee1193bfd1d58c
Shaddam Alghafiqih <shaddam.albz@gmail.com>
2022-11-27 14:41:26 +0700

    init

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import { FC, ReactNode } from 'react'
+
+export interface ButtonProps {
+  children: ReactNode
+}
+
+export const Button: FC<ButtonProps> = ({ children }) => {
+  return <button>{children}</button>
+}
